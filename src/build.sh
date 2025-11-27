4313bbadc0108250e8ef9ee0ed30c499e7cab18e
Delfi-CH <laspitzer@gmx.ch>
2025-11-27 12:07:30 +0100

    add all the build stuff

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build the Website for publishing
+# Run this in the Repositorys src directory
+
+cp ./index.html ../docs/index.html
+cp ./index.js ../docs/index.js
+cp ./index.css ../docs/index.css
+
+cp ../pdf_dokument/images/a.o._Gesamterneuerung-d.png ../docs/img/a.o._Gesamterneuerung-d.png
+cp ../pdf_dokument/images/Genehmigung_Staatsvertraege-d.png ../docs/img/Genehmigung_Staatsvertraege-d.png
+
+cp ../pdf_dokument/einfuehrungskurs_legislative.pdf ../docs/files/einfuehrungskurs_legislative.pdf
